c3cccdec11467977b362203546e8e210878c2aa5
BotCreating <105159846+necklessx@users.noreply.github.com>
2022-06-17 16:58:17 +0200

    Neckless & Artimxf

---
+++ README.md
@@ -0,0 +1,22 @@
+# Neckless & Artimxf Welcome Botlar
+
+- Sizlere geliştirmiş olduğum welcome botlarından biriyle tanıştırmak istiyorum.. 
+- Çok tokenli basit gelismis welcome bot.
+- Sağ üst kısımdan Star atarak destek olabilirsiniz.
+
+Asagidaki fotoda gostermis oldugum gibi tokenleri ve ses kanallarini azlatabilir veya coklatabilirsiniz.
+Ornek: "TOKEN_V":"sfsdjkfsd7fsdfjksdfjksdf9s993993h8834uhjfhdjkfjdf9.7743"
+Unutmayin ki 4 cu tokenin oldugu yerde , yazmalisiniz sona.
+
+![image](https://media.discordapp.net/attachments/975137206366785617/987367044515254284/unknown.png)
+
+He bide burda bak
+![image](https://media.discordapp.net/attachments/975137206366785617/987368060430192690/unknown.png)
+
+bak mesela ses degismek istiyosun . yazdigi yerde Kategori ismi gircen "Neckless Welcome Botlar" sonrada / sonrasinda sesin ismi yazmalisin dostum. Orn: **ses = await necklesscim.play('Neckless Welcome Botlar/konser.mp3');**
+
+Artimin githubu: https://github.com/Artimxf
+
+Daha fazla altyapi icin star at la
+
+optum bb
---
+++ neckless.js
@@ -0,0 +1,57 @@
+const Discord = require('discord.js');
+const ayarlar = require("./artimcim.json");
+
+
+const ozi = [];
+for (let index = 0; index < ayarlar.neckwelcometokenler.length; index++) {
+    const token = ayarlar.neckwelcometokenler[index];
+    const client = new Discord.Client();
+    client.login(token);
+
+    let artimcim;
+    client.on('ready', async () => {
+        setInterval(() => {
+        const welcomeaktivite = Math.floor(Math.random() * (ayarlar.botdinliyor.length));
+        client.user.setActivity(`${ayarlar.botdinliyor[welcomeaktivite]}`, {type: "LISTENING"});
+    }, 10000);
+        client.user.setStatus("idle"); ////////SEC BIRINI IDLE,DND,ONLINE
+        console.log(`${client.user.tag} olarak giriş yapıldı.`);
+        ozicik = await client.channels.cache.get(ayarlar.welcomeseskanalları[index]).join().catch(err => console.error("Botların Welcome Sesine Girerken Sorun Oluştu."));
+    });
+
+    let ses;
+    client.on('voiceStateUpdate', async (prev, cur) => {
+        if (cur.member.user.bot) return;
+        if (cur.channel && (cur.channel.id === ayarlar.welcomeseskanalları[index])) {
+            if (cur.channelID === prev.channelID) return;
+            if (neckless.includes(cur.member.id) && (cur.member.roles.highest.rawPosition < cur.guild.roles.cache.get(ayarlar.enaltyetkili).rawPosition)) {
+                ses = await artimcim.play('./welcome.mp3');
+                return;
+            }
+            if ((cur.member.roles.highest.rawPosition < cur.guild.roles.cache.get(ayarlar.enaltyetkili).rawPosition)) {
+                ses = await artimcim.play('./welcome.mp3');
+                neckless.push(cur.member.user.id);
+            } else if (cur.member.roles.highest.rawPosition > cur.guild.roles.cache.get(ayarlar.enaltyetkili).rawPosition) {
+                ses = await artimcim.play('./yetkili.mp3');
+                neckless.push(cur.member.user.id);
+            }
+        }
+        if (prev.channel && (prev.channel.id === ayarlar.welcomeseskanalları[index]) && (prev.channel.members.size === 1) && ses) ses.end();
+    });
+    
+    client.on('voiceStateUpdate', async (cur) => {
+        if (cur.member.id === client.user.id) ozicik = await client.channels.cache.get(ayarlar.welcomeseskanalları[index]).join();
+    })
+
+    client.on('voiceStateUpdate', async (___, newState) => {
+        if (
+        newState.member.user.bot &&
+        newState.channelID &&
+        newState.member.user.id == client.user.id &&
+        !newState.selfDeaf
+        ) {
+        newState.setSelfDeaf(true);
+        }
+        });
+
+}
